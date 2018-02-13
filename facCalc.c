da3398834cab69b4dfcf328b2bb597b7a5269bde
Adam <adam.lavely@gmail.com>
2018-02-13 15:37:09 -0500

    Update
    delete unneeded comments

--- facCalc.c
+++ facCalc.c
@@ -13,11 +13,9 @@
 int facCalc(int inputVal)
 {
 	int counter, output;
-	//output=0;
 
 	// Calculate the factorial
 	for (counter=1; counter<inputVal; counter++)
-		// Line 20
 		output=output*counter;    
 
 	// Return the variable to the main function
